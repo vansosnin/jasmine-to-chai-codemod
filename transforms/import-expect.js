f36a6e087e29ca2a1a7ea5af12b1d08bf4d9c281
Rene Saarsoo <nene@triin.net>
2017-12-18 21:56:11 +0200

    Extract check for existance of import to function

--- transforms/import-expect.js
+++ transforms/import-expect.js
@@ -4,25 +4,28 @@ export default function transformer(file, api) {
 
     const root = j(file.source);
 
-    const importExpectChaiDeclarations = root.find(j.ImportDeclaration, {
-        specifiers: [{
-            local: {
-                name: 'expect'
-            }
-        }],
-        source: {
-            value: 'chai'
-        }
-    });
-    const hasImportExpectChai = importExpectChaiDeclarations.__paths.length > 0;
-
     return root
         .find(j.Program)
         .replaceWith(({value: node}) => {
-            if (!hasImportExpectChai) {
+            if (!hasImportExpect(root)) {
                 node.body.unshift("import { expect } from 'chai';");
             }
             return node;
         })
         .toSource();
+
+    function hasImportExpect(root) {
+        const declarations = root.find(j.ImportDeclaration, {
+            specifiers: [{
+                local: {
+                    name: 'expect'
+                }
+            }],
+            source: {
+                value: 'chai'
+            }
+        });
+
+        return declarations.__paths.length > 0;
+    }
 };
