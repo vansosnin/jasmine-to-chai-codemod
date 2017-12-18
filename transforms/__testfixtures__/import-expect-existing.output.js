a58d4d5de5b89d9921f25b7280c9e97efc1e3c19
Rene Saarsoo <nene@triin.net>
2017-12-18 22:03:42 +0200

    Additional test for import-expect, when import already exists

---
+++ transforms/__testfixtures__/import-expect-existing.output.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+describe("import-expect transforms", function() {
+    it("performs tests", function() {
+        expect(foo).something(bar);
+    });
+});
--- transforms/__tests__/import-expect-test.js
+++ transforms/__tests__/import-expect-test.js
@@ -1,3 +1,4 @@
 import {defineTest} from 'jscodeshift/dist/testUtils';
 
 defineTest(__dirname, 'import-expect');
+defineTest(__dirname, 'import-expect', null, 'import-expect-existing');
