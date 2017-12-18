1e00b94f14018432590c1578018ae988782ed161
Rene Saarsoo <nene@triin.net>
2017-12-18 21:50:59 +0200

    Decrease indentation in functions transform
    Like previously done in base-transfroms transform.

--- transforms/functions.js
+++ transforms/functions.js
@@ -3,22 +3,20 @@ export default function transformer(file, api) {
 
     return j(file.source)
         .find(j.Identifier)
-        .replaceWith(
-            ({value: node}) => {
-                switch (node.name) {
-                    case 'toBe':
-                        return 'to.equal';
-                    case 'toEqual':
-                        return 'to.deep.equal';
-                    case 'toMatch':
-                        return 'to.match';
-                    case 'toContain':
-                        return 'to.contain';
+        .replaceWith(({value: node}) => {
+            switch (node.name) {
+                case 'toBe':
+                    return 'to.equal';
+                case 'toEqual':
+                    return 'to.deep.equal';
+                case 'toMatch':
+                    return 'to.match';
+                case 'toContain':
+                    return 'to.contain';
 
-                    default:
-                        return node;
-                }
+                default:
+                    return node;
             }
-        )
+        })
         .toSource();
 };
