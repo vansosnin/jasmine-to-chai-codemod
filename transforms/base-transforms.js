3f683f295d9d63659f71f0eb95cda3af431734d1
Rene Saarsoo <nene@triin.net>
2017-12-19 12:45:47 +0200

    Extract parts of transform chain to function and pattern constant

--- transforms/base-transforms.js
+++ transforms/base-transforms.js
@@ -1,55 +1,59 @@
+const expectPattern = {
+    expression: {
+        type: 'CallExpression',
+        callee: {
+            type: 'MemberExpression',
+            object: {
+                type: 'CallExpression',
+                callee: {
+                    type: 'Identifier',
+                    name: 'expect'
+                }
+            }
+        }
+    }
+};
+
 export default function transformer(file, {jscodeshift: j}) {
     const { statement } = j.template;
 
     return j(file.source)
-        .find(j.ExpressionStatement, {
-            expression: {
-                type: 'CallExpression',
-                callee: {
-                    type: 'MemberExpression',
-                    object: {
-                        type: 'CallExpression',
-                        callee: {
-                            type: 'Identifier',
-                            name: 'expect'
-                        }
-                    }
-                }
-            }
-        })
+        .find(j.ExpressionStatement, expectPattern)
         .replaceWith(({value: node}) => {
             const fnCall = node.expression;
-            const [expectArg] = fnCall.callee.object.arguments;
-
-            switch(fnCall.callee.property.name) {
-                case 'toBeFalsy':
-                    return statement`expect(${expectArg}).not.to.be.ok;`;
-                case 'toBeTruthy':
-                    return statement`expect(${expectArg}).to.be.ok;`;
-                case 'toBeUndefined':
-                    return statement`expect(${expectArg}).to.be.undefined;`;
-                case 'toBeDefined':
-                    return statement`expect(${expectArg}).to.not.be.undefined;`;
-                case 'toBeNull':
-                    return statement`expect(${expectArg}).to.be.null;`;
-                case 'toThrow':
-                    // toThrow()                          -> to.throw()                   (!!)
-                    // toThrow("msg")                     -> to.throw("msg")
-                    // toThrow(TypeError)                 -> to.throw(TypeError)
-                    // toThrow(new TypeError("msg"))      -> to.throw(TypeError, "msg")
-                    return transformThrow(expectArg, toThrowArgs(fnCall.arguments));
-                case 'toThrowError':
-                    // toThrowError()                     -> to.throw(Error)              (!!)
-                    // toThrowError("msg")                -> to.throw("msg")
-                    // toThrowError(TypeError)            -> to.throw(TypeError)
-                    // toThrowError(new TypeError("msg")) -> to.throw(TypeError, "msg")
-                    // toThrowError(TypeError, "msg")     -> to.throw(TypeError, "msg")   (!!)
-                    return transformThrow(expectArg, toThrowErrorArgs(fnCall.arguments));
-                default:
-                    return node;
-            }
+            return transformExpect(fnCall, fnCall.callee.object.arguments)
         })
         .toSource();
+
+    function transformExpect(fnCall, [expectArg]) {
+        switch(fnCall.callee.property.name) {
+            case 'toBeFalsy':
+                return statement`expect(${expectArg}).not.to.be.ok;`;
+            case 'toBeTruthy':
+                return statement`expect(${expectArg}).to.be.ok;`;
+            case 'toBeUndefined':
+                return statement`expect(${expectArg}).to.be.undefined;`;
+            case 'toBeDefined':
+                return statement`expect(${expectArg}).to.not.be.undefined;`;
+            case 'toBeNull':
+                return statement`expect(${expectArg}).to.be.null;`;
+            case 'toThrow':
+                // toThrow()                          -> to.throw()                   (!!)
+                // toThrow("msg")                     -> to.throw("msg")
+                // toThrow(TypeError)                 -> to.throw(TypeError)
+                // toThrow(new TypeError("msg"))      -> to.throw(TypeError, "msg")
+                return transformThrow(expectArg, toThrowArgs(fnCall.arguments));
+            case 'toThrowError':
+                // toThrowError()                     -> to.throw(Error)              (!!)
+                // toThrowError("msg")                -> to.throw("msg")
+                // toThrowError(TypeError)            -> to.throw(TypeError)
+                // toThrowError(new TypeError("msg")) -> to.throw(TypeError, "msg")
+                // toThrowError(TypeError, "msg")     -> to.throw(TypeError, "msg")   (!!)
+                return transformThrow(expectArg, toThrowErrorArgs(fnCall.arguments));
+            default:
+                return node;
+        }
+    }
 
     function toThrowArgs([arg]) {
         if (!arg) {
